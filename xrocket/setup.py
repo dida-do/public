277aa39b4fe8da886aac83a992f5aa822fe356ea
felixbrunner <rubelrennfix@gmail.com>
2023-08-01 16:29:13 +0200

    set up basic package files

---
+++ xrocket/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="xrocket",
+    version="0.1",
+    description="explainable rocket implementation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="dida Datenschmiede GmbH",
+    author_email="info@dida.do",
+    packages=find_packages(),
+    install_requires=[
+        "ipython",
+        "ipywidgets",
+    ],
+    python_requires=">=3.6",
+)
